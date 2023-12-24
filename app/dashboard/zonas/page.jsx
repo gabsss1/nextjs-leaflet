fcef06243a341fc30b988ea9d9796f2c6cc2cc12
gabsss1 <202011422@urp.edu.pe>
2023-12-24 15:38:33 -0500

    Zonas CRUD

--- app/dashboard/zonas/page.jsx
+++ app/dashboard/zonas/page.jsx
@@ -1,6 +1,18 @@
+import styles from '../../ui/dashboard/zonas/zonas.module.css'
+import Search from '../../ui/dashboard/search/search'
+import Link from 'next/link'
+
 const ZonasPage = () => {
     return (
-        <div>Zona Page</div>
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Buscar zonas" />
+                <Link href="dashboard/zonas/add">
+                    <button className={styles.button}>Agregar zona</button>
+                </Link>
+            </div>
+            <table className={styles.table}></table>
+        </div>
     )
 }
 
